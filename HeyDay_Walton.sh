b64db76ca39b5b3e53070b4755a0769a334added
hmw0069 <114432405+hmw0069@users.noreply.github.com>
2022-10-11 11:02:02 -0500

    Add files via upload

---
+++ HeyDay_Walton.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#Beginning of script. Script prompts user to enter their, then reads the 
+name the user entered and says hey to that name.
+
+echo "Please input your name."
+
+read name
+
+echo "Hey $name!"
+
+#Script is finished. Output says Hey to the name that the user entered.
